1d8f505d5061b044e1352e37114a8a5c323b2e6b
Abhishek Kange <84184526+Abhishekkange@users.noreply.github.com>
2025-11-22 11:47:26 +0530

    dotenv removed for vercel deploy test

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-require('dotenv').config();
+// require('dotenv').config();
 const dbconnect = require('./api/v1/db/db')
 app.use(express.json());
 const axios = require('axios')
